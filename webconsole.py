902c9f42860d5b31423aa9bed5b200e36949b5ab
Ali Zaidi <alixedi@gmail.com>
2018-08-08 11:23:14 +0100

    Putting in some basic ideas to expand upon

---
+++ webconsole.py
@@ -0,0 +1,32 @@
+import sys, code, io, json, contextlib
+
+import flask
+
+
+app = flask.Flask(__name__)
+app.consoles = {}
+
+
+class WebConsole():
+
+    def __init__(self):
+        self.console = code.InteractiveConsole()
+
+    def run(self, code):
+        output = io.StringIO()
+        with contextlib.redirect_stdout(output):
+            with contextlib.redirect_stderr(output):
+                self.console.push(code)
+        return {'output': str(output.getvalue())}
+
+
+@app.route('/run/<uname>', methods=['POST'])
+def run(uname):
+    if not uname in app.consoles:
+        app.consoles[uname] = WebConsole()
+    return flask.jsonify( 
+        app.consoles[uname].run(
+            flask.request.get_json()['input']
+        )
+    )
+
